d2a13a2e849ef8d4d44986f40d3d08e65f44f4bd
agungricky07 <agungricky07@gmail.com>
2023-10-29 13:01:20 +0700

    Upload 1.2 : Pindah Halaman selesai

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.android.spk_sawi"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.android.spk_sawi"
--- app/src/main/java/com/android/spk_sawi/MainActivity.kt
+++ app/src/main/java/com/android/spk_sawi/MainActivity.kt
@@ -2,10 +2,20 @@ package com.android.spk_sawi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button) // Ganti 'button' dengan ID yang sesuai dengan elemen button Anda
+
+        button.setOnClickListener {
+            val intent = Intent(this, form::class.java) // Ganti 'SecondActivity' dengan nama kelas aktivitas kedua
+            startActivity(intent)
+        }
     }
 }
