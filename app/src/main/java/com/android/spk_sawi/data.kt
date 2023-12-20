cf7f9bf4e3566be323a1e9c4a375c82d52dc5412
agungricky07 <agungricky07@gmail.com>
2023-12-20 14:19:19 +0700

    Upload Ricky (Api Get sudah bisa jalan)

--- app/src/main/java/com/android/spk_sawi/data.kt
+++ app/src/main/java/com/android/spk_sawi/data.kt
@@ -2,10 +2,38 @@ package com.android.spk_sawi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class data : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data)
+
+        performGetRequest()
+    }
+
+    private fun performGetRequest(){
+        val q = Volley.newRequestQueue(this)
+        val url = "http://192.168.43.126/API_Server/api/alldata"
+
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> {
+                Log.d("apiresult", it)
+                // Tampilkan respons di TextView
+                findViewById<TextView>(R.id.tvApiResponse).text = it
+            },
+            Response.ErrorListener {
+                Log.e("apiresult", it.message.toString())
+                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+            })
+
+        q.add(stringRequest)
     }
 }
